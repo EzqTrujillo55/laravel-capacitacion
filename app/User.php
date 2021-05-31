7eb5ba87a6b3119a76ec323ec83954e95bc5776c
PabloTrujillo8 <pablo@vantageio.com>
2021-05-31 18:03:51 -0500

    Instalamos jwt auth, creamos los metodos en el modelo de User y los metodos en el controlador de User

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject; //Agregar JWT a nuestro modelo de usuario
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -36,4 +37,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    //Obtenemos identificador del JWT
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    //Obtenemos los métodos para generar el token, [] => 'default'
+    public function getJWTCustomClaims(){
+        return [];
+    }
 }
